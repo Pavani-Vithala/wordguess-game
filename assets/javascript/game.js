9897aae96122dbea0d96faba96d8fa273149b825
Pavani Vithala <pavani.vithala18@gmail.com>
2018-10-09 22:03:22 -0400

    draft 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,61 @@
 
-   
-    var veggieList = ["carrots", "beans", "peas","potato","spinach","tomatoes"];
 
-    // Creating variable to hold the number of wins
-    var wins = 0;
-    
-
-    // Create variables that hold references to the places in the HTML where we want to display things.
-    var WinCount = document.getElementById("WinCount");
-   var WordChoice = document.getElementById("WordChoice");
-    var GuessesLeft = document.getElementById("GuessesLeft");
-    var LettersGuessed = document.getElementById("LettersGuessed");
-    var GuessedSoFar=[];
-    //LettersGuessed=document.getElementById("LettersGuessed");
-   
-
-    // This function is run whenever the user presses a key.
-    window.addEventListener('load', function() {
-    
-    var GuessesLeft = veggieList.length;
-    var WinCount = wins;
+// This function is run onload of the page
+window.onload = function () {
+    var veggieList = ["carrots", "beans", "peas", "potato", "spinach", "tomatoes"];
     var CurrentWord = veggieList[Math.floor(Math.random() * veggieList.length)];
-    console.log("CurrentVeggie is "+CurrentWord);
-    console.log("lenght of veggie is  "+CurrentWord.length);
+    var GuessesLeft=10;
+    var wins = document.getElementById("WinCount");
+    //var ChosenWord = document.getElementById("WordChoice");
+    //var GuessCounter = document.getElementById("GuessesLeft");
+    //GuessCounter = GuessesLeft;
+   // var GuessedSofar = document.getElementById("LettersGuessed");
+    document.getElementById("GuessesLeft").innerHTML = GuessesLeft;
+    document.getElementById("WinCount").innerHTML = 0;
+    console.log("CurrentVeggie is " + CurrentWord);
+    console.log("length of veggie is  " + CurrentWord.length);
+    console.log("GuessesLeft is " + GuessesLeft)
+    // console.log("WinsCount "+wins);
+    var WordChoice = [];
+    var LetterGuessed = [];
     
-    /*for (var i=0;i<CurrentWord.length;i++)
-    {
-        //var nextChar = str.charAt(i);
-        var btn= document.createElement("button");
-        btn[i] = btn[i]+str[i];
-        //console.log(btn[i]);
-       
-        document.getElementById("WordChoice").textContent = btn;
-
-    }*/
+    var Space = [""]
+    for (var i = 0; i < CurrentWord.length; i++) {
         
-  for (i=0;i<GuessesLeft;i++)
-   {
-    document.onkeyup = function(event) {
-
-      // Determines which key was pressed.
-      var userGuess = event.key;
-      console.log(userGuess);
-      
-      for(j=0;j<CurrentWord.length;j++)
-      {
-      if  (userGuess === CurrentWord[j])
-        
-         WordChoice[j] = CurrentWord[j];
+           
+            WordChoice[i] = "-"
+           
        
-        else
-            LettersGuessed[j] = userGuess;
-
-            GuessesLeft = (GuessesLeft -1);
-
-      }
-            
-     }
     }
-      
-      });
- 
+    document.getElementById("WordChoice").innerHTML = WordChoice;
+    
+    console.log("The Word Choice is  "+WordChoice);
+    
+    document.onkeyup = function (event) {
+
+        // Determines which key was pressed.
+        var userGuess = event.key;
+        console.log(userGuess);
+
+        for (var j = 0; j < CurrentWord.length; j++) {
+            if (userGuess === CurrentWord[j]) {
+                WordChoice[j] = userGuess;
+                document.getElementById("WordChoice").innerHTML = WordChoice;
+                //WordChoice[j] = CurrentWord[j];
+                document.getElementById("GuessesLeft").innerHTML=GuessesLeft;
+            }
+            else {
+                LettersGuessed[j] = userGuess;
+                document.getElementById("LettersGuessed").innerHTML = LettersGuessed[j];
+                document.getElementById("GuessesLeft").innerHTML=GuessesLeft;
+            }
+
+            GuessesLeft=Guessesleft-1;
+
+        }
+
+    };
+
+
+};
+
