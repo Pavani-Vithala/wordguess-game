c4f7c2860b9771a141e4c5676a56dc11e40e639e
Pavani Vithala <pavani.vithala18@gmail.com>
2018-10-13 02:30:45 -0400

    Fixing counter issue

--- assets/javascript/game1.js
+++ assets/javascript/game1.js
@@ -8,15 +8,20 @@ window.onload = function () {
     var LetterGuessed = [];
     var GuessCounter = 10;
     document.getElementById("WinCount").innerHTML = wins;
+    document.getElementById("GuessesLeft").innerHTML = GuessCounter;
     CurrentWord = Refresh();
+
     document.onkeyup = function (event) {
         // Determines which key was pressed.
         var userGuess = event.key;
-        console.log(userGuess);
+        if (GuessCounter == null) {
+            GuessCounter = 10;
+            GuessCounter = GuessCounter - 1;
+        }
+        else
+            GuessCounter = GuessCounter - 1;
 
-
-
-        //GuessesLeft = GuessCounter;
+        document.getElementById("GuessesLeft").innerHTML = GuessCounter;
         var t = 0;
         console.log("Entered Keypress function and current word is :" + CurrentWord);
         for (var j = 0; j < CurrentWord.length; j++) {
@@ -28,85 +33,80 @@ window.onload = function () {
             }
         }
 
-        if (t !== 1) {
+        if (t != 1) {
             LetterGuessed.push(userGuess);
             document.getElementById("LettersGuessed").innerHTML = LetterGuessed;
             console.log("The Letters Guessed is :" + LetterGuessed);
         }
-
-        GuessCounter = GuessCounter - 1;
+        console.log("The guesscntr is" + GuessCounter);
+        //GuessCounter = GuessCounter - 1;
         document.getElementById("GuessesLeft").innerHTML = GuessCounter;
 
-        console.log("Guess Counter is  " +GuessCounter);
-        console.log("Word Choice is " +WordChoice);
-        console.log("CurrentWord is " +CurrentWord);
+        console.log("Guess Counter is  " + GuessCounter);
+        console.log("Word Choice is " + WordChoice);
+        console.log("CurrentWord is " + CurrentWord);
         //var check = 0;
         //var img = document.getElementById("veggiepic").innerHTML;
         var check1 = 0;
         var check2 = 0;
-        for (i = 0; i < WordChoice.length; i++) {
+        for (var i = 0; i < WordChoice.length; i++) {
             if (WordChoice[i] == "-") {
                 check1 = check1 + 1;
 
             }
             else
                 check2 = check2 + 1;
+
         }
+
 
         //console.log("The value of check is " + check);
         if (check1 >= 1 && GuessCounter == 0) {
             document.getElementById("WinCount").innerHTML = wins;
             console.log("Your Wincount is :" + wins);
-           // Refresh(CurrentWord);
-            exit();
+            CurrentWord = Refresh();
+
+        } else {
+
+            if (check2 >= 1 && check1 == 0) {
+                console.log("Entered the CounterCheck if"); console.log("Check1 is " + check1);
+                console.log("Entered the CounterCheck if"); console.log("Check2 is " + check2);
+                wins = wins + 1;
+                //sessionStorage.setItem("wins", parseInt(wins));
+                document.getElementById("WinCount").innerHTML = wins;
+                var GuessCounter = 10;
+                CurrentWord = Refresh();
+
+            }
+
         }
-
-        if (check2 == 1 && check1 == 0) {
-            console.log("Entered the CounterCheck if");console.log("Check1 is "+check1);
-            console.log("Entered the CounterCheck if");console.log("Check2 is "+check2);
-            wins = wins + 1;
-            //sessionStorage.setItem("wins", parseInt(wins));
-            document.getElementById("WinCount").innerHTML = wins;
-            GuessCounter = 10;
-            Refresh(CurrentWord);
-
-        }
-
 
         document.getElementById("Exit").onclick = function () {
 
             var exit = prompt("Are you sure you want to stop the fun?(Y/N)");
             if (exit == "Y") {
-                alert("good bye.See you soon");
-                close();
+                //alert("good bye.See you soon");
+                window.close();
             }
-            else
-                Refresh(CurrentWord);
-
-
+            else {
+                var GuessCounter = 10;
+                CurrentWord = Refresh();
+            }
         }
-        
 
-        /*function getImage(str1){
-        
-        var x = document.createElement("IMG");
-            if (str1=="beans")
-            {
-                x.setAttribute("src","assets/images/beans.jpg");
-            }
-        return(x);*/
     }
     function Refresh() {
         console.log("Entered Refresh function:");
         var veggieList = ["carrots", "beans", "peas", "potato", "spinach", "tomatoes"];
         var CurrentWord = veggieList[Math.floor(Math.random() * veggieList.length)];
-        //var GuessCounter = 10;
+        var GuessCounter = 10;
+        console.log("The counter in Refresh functrion is " + GuessCounter);
         LetterGuessed = [];
         WordChoice = [];
         for (var i = 0; i < CurrentWord.length; i++) {
             WordChoice[i] = "-"
         }
-        var GuessCounter = 10;
+        //var GuessCounter = 10;
         document.getElementById("LettersGuessed").innerHTML = LetterGuessed;
         document.getElementById("GuessesLeft").innerHTML = GuessCounter;
         document.getElementById("WordChoice").innerHTML = WordChoice;
